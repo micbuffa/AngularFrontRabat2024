5c3fdbe51eaf527884b0072db1646e9b2aa33e5a
micbuffa <micbuffa@gmail.com>
2024-01-11 11:04:03 +0100

    avec ajout

--- src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -3,10 +3,13 @@ import { Assignment } from '../assignment.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-assignment-detail',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatCheckboxModule],
+  imports: [CommonModule, MatCardModule, MatCheckboxModule, MatButtonModule,
+    MatIconModule],
   templateUrl: './assignment-detail.component.html',
   styleUrl: './assignment-detail.component.css'
 })
